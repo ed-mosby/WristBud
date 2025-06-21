1ef4d63ac98f8a62e8e456ad0e0fba0ae231f18b
gelocollins <gelocollins@icloud.com>
2025-06-21 16:35:19 +0800

    updatd

--- App.tsx
+++ App.tsx
@@ -1,13 +1,6 @@
 
 import React from 'react';
 import HealthPredictor from './components/HealthPredictor';
-
-const App: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-slate-900 flex flex-col items-center p-4">
-      <HealthPredictor />
-    </div>
-  );
+const App: React.FC = () => { return ( <div className="min-h-screen bg-slate-900 flex flex-col items-center p-4"> <HealthPredictor /> </div> );
 };
-
 export default App;
